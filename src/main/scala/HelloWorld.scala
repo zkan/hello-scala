97e9ecc52f957d2f2374c78efc06d78f9c6cce05
zkan <kan@prontomarketing.com>
2018-08-12 14:31:08 +0700

    Play with multiply functions :bear:

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,14 @@
+
 object HelloWorld {
+  def multipliedByTwo(x: Int): Int = if (x == 0) x else x * 2
+
+  def multipliedByTen(x: Double = 10, y: Double): Double = {
+      x * y
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello World")
+    println(multipliedByTwo(5))
+    println(multipliedByTen(y=9))
   }
 }
